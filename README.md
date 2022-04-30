30ba0c683bb62817385ff303241d11b6eefd89cf
Haguermeister <cadethague70@gmail.com>
2022-04-30 15:35:58 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# House-show
+# House Show
+
+A booking application for venue owners and artists.
+
+## Github Repo Link : https://github.com/Haguermeister/House-show
+
+## Webpage Link : https://houseshownow.com
+
+## Screenshot Below :
+
+![alt text](/public/images/ScreenShot.png)
+
+---
+
+## User Story :
+
+---
+
+#### AS AN aspiring musician or venue owner
+
+#### I WANT AN application to provide me an intuitive user interface for booking artists or reserving venues
+
+#### SO THAT I can show case my talents and or attract customers to me space
+
+---
+
+#### GIVEN
+
+#### WHEN
+
+#### THEN
---
+++ client/build/precache-manifest.575b47f4554e00ce3c92b8584a27b023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f71b2165cc19ecfa04c2852cf628de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cea8e6380b244254ab0",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "587eb6f62ebbf3491a4d",
+    "url": "/static/js/2.2bfaa6a8.chunk.js"
+  },
+  {
+    "revision": "cf1756f17a9fe3b6b59b94401b81c70b",
+    "url": "/static/js/2.2bfaa6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cea8e6380b244254ab0",
+    "url": "/static/js/main.05b636bf.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
