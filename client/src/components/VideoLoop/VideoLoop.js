2535d7a394aa9e67a0677ce7cd958e500e012443
Alan Phelps <phelpsa64@gmail.com>
2022-05-02 19:56:14 -0400

    heroku

--- client/src/components/VideoLoop/VideoLoop.js
+++ client/src/components/VideoLoop/VideoLoop.js
@@ -1,11 +1,11 @@
 import React from "react";
-import backgroundVideo from "./public/video/backgroundVideo.mp4";
+// import backgroundVideo from "./public/video/backgroundVideo.mp4";
 
 const VideoLoop = () => {
   return (
-    <video autoplay loop muted id="video">
-      <source src={backgroundVideo} type="video/mp4" />
-    </video>
+    // <video autoplay loop muted id="video">
+    //   <source src={backgroundVideo} type="video/mp4" />
+    // </video>
   );
 };
 export default VideoLoop;
