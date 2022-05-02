cc59899f56533e4c4502ae8f8d6ce869b7eb3c66
Haguermeister <cadethague70@gmail.com>
2022-05-02 18:57:47 -0400

    Home Page

---
+++ client/src/components/VideoLoop/VideoLoop.js
@@ -0,0 +1,11 @@
+import React from "react";
+import backgroundVideo from "./public/video/backgroundVideo.mp4";
+
+const VideoLoop = () => {
+  return (
+    <video autoplay loop muted id="video">
+      <source src={backgroundVideo} type="video/mp4" />
+    </video>
+  );
+};
+export default VideoLoop;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import VideoLoop from "../components/VideoLoop/VideoLoop";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Home = () => {
-  return <div className="">We are going to crush this!</div>;
+  return (
+    <div className="">
+      <p>Alan can you add Auth While i Style this page?</p>
+      <VideoLoop />
+      <Link to={{ pathname: "/explore" }}>Explore</Link>
+      <Link to={{ pathname: "/login" }}>I want to Host</Link>
+      <Link to={{ pathname: "/login" }}>I want to Perform</Link>
+    </div>
+  );
 };
 export default Home;
