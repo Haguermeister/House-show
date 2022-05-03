b67070678063294ac046ba7abb59905ff3df0050
whosshazo <ryandeshazo1@gmail.com>
2022-05-03 19:33:54 -0400

    deleted login component

---
+++ client/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import musician from "../../../public/images/musician";
+
+const Login = () => {
+  return <div> Hello</div>;
+};
+
+export default Login;
