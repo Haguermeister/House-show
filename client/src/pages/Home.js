4ea227aec389124567fd4e4993dbc674d36b4385
Haguermeister <cadethague70@gmail.com>
2022-05-02 20:40:12 -0400

    Test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import VideoLoop from "../components/VideoLoop/VideoLoop";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Home.css";
 const Home = () => {
   return (
     <div className="">
-      <p>Alan can you add Auth While i Style this page?</p>
       <VideoLoop />
-      <Link to={{ pathname: "/explore" }}>Explore</Link>
-      <Link to={{ pathname: "/login" }}>I want to Host</Link>
-      <Link to={{ pathname: "/login" }}>I want to Perform</Link>
+      <Link className="btn button p-2" to={{ pathname: "/explore" }}>
+        Explore
+      </Link>
+      <Link className="btn button p-2" to={{ pathname: "/login" }}>
+        I want to Host
+      </Link>
+      <Link className="btn button p-2" to={{ pathname: "/login" }}>
+        I want to Perform
+      </Link>
     </div>
   );
 };
