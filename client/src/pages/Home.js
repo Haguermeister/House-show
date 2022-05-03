6e86d602008f7b89843d8a98b2d025c77ab51f18
whosshazo <ryandeshazo1@gmail.com>
2022-05-03 18:31:05 -0400

    adding Signin link to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import VideoLoop from "../components/VideoLoop/VideoLoop";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Auth from "../utils/auth"
+import Auth from "../utils/auth";
 const Home = () => {
   return (
     <div className="">
       <VideoLoop />
-      {Auth.loggedIn() && (<Link to={{ pathname: "/explore" }}>Explore</Link>)}
+      {Auth.loggedIn() && <Link to={{ pathname: "/explore" }}>Explore</Link>}
       <Link to={{ pathname: "/login" }}>I want to Host</Link>
       <Link to={{ pathname: "/login" }}>I want to Perform</Link>
+      <Link to={{ pathname: "/SignIn" }}>Sign in</Link>
     </div>
   );
 };
