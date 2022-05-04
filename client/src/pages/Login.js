5be41d8eeda32b5153ab0a4a07aa3622d7269668
whosshazo <ryandeshazo1@gmail.com>
2022-05-03 22:22:30 -0400

    overlay sign in text

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "./Login.css";
 import Musician from "../assets/musician.jpg";
 
 const Login = () => {
   return (
-    <section className="background">
-      
-      <img src={Musician} 
-      alt="folk singer"
-      style={{ width: "100%", height: "100%"}} />
+    <section className="loginIn">
+      <div className="media ">
+        <img
+          className="backgroundImage"
+          src={Musician}
+          alt="folk singer"
+          style={{ width: "100%", height: "100%" }}
+        />
+        <h1 className="overlayText">Sign in</h1>
+
+        
+      </div>
     </section>
   );
 };
