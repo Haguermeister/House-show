36d25e13ae2806a2ac9163507a2e81099fa2e8a5
whosshazo <ryandeshazo1@gmail.com>
2022-05-04 19:11:08 -0400

    fixing git

--- client/src/components/ArtistCard/ArtistCard.js
+++ client/src/components/ArtistCard/ArtistCard.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 import { Carousel } from "react-responsive-carousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import "./ArtistCard.css";
 
 const ArtistCard = (props) => {
+  const saveArtist = () => {};
   return (
-    <div className="">
+    <div>
       {props.artistsData.map((artist) => {
         return (
           <div
-            className="d-flex justify-content-center flex-column aligin-items-center  pt-2"
+            className="d-flex justify-content-center flex-column aligin-items-center"
             key={artist.id}
           >
+            <FontAwesomeIcon
+              onClick={saveArtist}
+              size="xs"
+              className="heartButton"
+              icon={faHeartCirclePlus}
+            />
             <Carousel
               className="carousel mx-auto"
               showThumbs={false}
@@ -20,27 +29,23 @@ const ArtistCard = (props) => {
             >
               {artist.pictures.map((picture) => {
                 return (
-                  <div>
-                    <img
-                      className="imgCarousel"
-                      alt="artist pictures"
-                      src={picture}
-                    />
-                    {/*"./images/venue2.png"*/}
-                  </div>
+                  <img
+                    className="imgCarousel"
+                    alt="artist pictures"
+                    src={picture}
+                  />
                 );
               })}
             </Carousel>
-
-            <div className="d-flex justify-content-around pt-3">
+            <div className="d-flex justify-content-around pt-3 bookings-text">
               <div>
-                <h2>{artist.Name}</h2>
-                <h3>$ {artist.Rate}</h3>
+                <h2 className="h2">{artist.Name}</h2>
+                <h3 className="h3">${artist.Rate}</h3>
               </div>
 
               <div>
-                <h3>{artist.musicType}</h3>
-                <h3>{artist.bandSize}</h3>
+                <h2 className="h2">{artist.musicType}</h2>
+                <h3 className="h3">{artist.bandSize}</h3>
               </div>
             </div>
           </div>
