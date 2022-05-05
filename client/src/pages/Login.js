6f298828302b24f1fd6a4c9de9409e7f75512865
whosshazo <ryandeshazo1@gmail.com>
2022-05-04 22:00:27 -0400

    login page with radio buttons

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import "./Login.css";
+import Form from "react-bootstrap/Form";
 import Musician from "../assets/musician.jpg";
+import "./Login.css";
+
+
 
 const Login = () => {
   return (
@@ -13,6 +16,37 @@ const Login = () => {
           style={{ width: "100%", height: "100%" }}
         />
         <h1 className="overlayText">Sign in</h1>
+
+        <div className="userCheck">
+          <Form>
+            <Form.Check type="switch" id="hostSwitch" label="Im a Host" />
+
+            <Form.Check type="switch" id="artistSwitch" label="Im an Artist" />
+          </Form>
+        </div>
+
+        <div className="userLogin">
+          <>
+            <Form.Label htmlFor="loginEmail">Email</Form.Label>
+            <Form.Control
+              type="email"
+              id="inputEmail"
+              aria-describedby="passwordHelpBlock"
+              
+
+            />
+            <Form.Text id="passwordHelpBlock" muted></Form.Text>
+
+            <Form.Label htmlFor="loginPassword">Password</Form.Label>
+            <Form.Control
+              placeholder="******"
+              type="passworrd"
+              id="inputPassword"
+              
+              aria-describedby="passwordHelpBlock"
+            />
+          </>
+        </div>
       </div>
     </section>
   );
