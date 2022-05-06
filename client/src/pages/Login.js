013afe04a4568196f2d1acc356f8f37523baaba4
whosshazo <ryandeshazo1@gmail.com>
2022-05-06 09:37:10 -0400

    Login page updates

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
 import Form from "react-bootstrap/Form";
 import Musician from "../assets/musician.jpg";
+import { LOGIN_ARTIST } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
-
-
 const Login = () => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN_ARTIST);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <section className="loginIn">
       <div className="media ">
@@ -18,33 +44,45 @@ const Login = () => {
         <h1 className="overlayText">Sign in</h1>
 
         <div className="userCheck">
-          <Form>
+          {/* <Form>
             <Form.Check type="switch" id="hostSwitch" label="Im a Host" />
 
             <Form.Check type="switch" id="artistSwitch" label="Im an Artist" />
-          </Form>
+          </Form> */}
         </div>
 
         <div className="userLogin">
           <>
-            <Form.Label htmlFor="loginEmail">Email</Form.Label>
-            <Form.Control
-              type="email"
-              id="inputEmail"
-              aria-describedby="passwordHelpBlock"
-              
+            <Form onSubmit={handleFormSubmit}>
+              <Form.Label htmlFor="loginEmail"></Form.Label>
+              <Form.Control
+                placeholder="yourname@email.com"
+                type="email"
+                id="inputEmail"
+                onChange={handleChange}
+                aria-describedby="passwordHelpBlock"
+              />
+              <Form.Text id="passwordHelpBlock" muted></Form.Text>
 
-            />
-            <Form.Text id="passwordHelpBlock" muted></Form.Text>
-
-            <Form.Label htmlFor="loginPassword">Password</Form.Label>
-            <Form.Control
-              placeholder="******"
-              type="passworrd"
-              id="inputPassword"
-              
-              aria-describedby="passwordHelpBlock"
-            />
+              <Form.Label htmlFor="loginPassword"></Form.Label>
+              <Form.Control
+                placeholder="******"
+                type="passworrd"
+                id="inputPassword"
+                onChange={handleChange}
+                aria-describedby="passwordHelpBlock"
+              />
+            </Form>
+            {error ? (
+              <div>
+                <p className="error-text">
+                  The provided credentials are incorrect
+                </p>
+              </div>
+            ) : null}
+            <div className="flex-row flex-end submitButton">
+              <button type="submit">Sign in</button>
+            </div>
           </>
         </div>
       </div>
