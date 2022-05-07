5ccfada0b3cf50fa32073663cf4fea7dc324e592
whosshazo <ryandeshazo1@gmail.com>
2022-05-07 14:19:34 -0400

    modal functionality in play

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-function Modal () {
-    return (
-        <div>
+function Modal({ closeModal }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <button onClick={() => closeModal(false)}> X </button>
 
+        <div className="title">
+          <h1>Artist Signup</h1>
         </div>
-    );
-};
+
+        <div className="body">
+          <p>Form Form Form</p>
+        </div>
+
+        <div className="footer">
+          <button onClick={() => closeModal(false)}>Cancel</button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Modal;
