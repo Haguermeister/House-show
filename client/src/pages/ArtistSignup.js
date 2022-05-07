05aae869f88307db3f65a4b753c3696a22e94ece
whosshazo <ryandeshazo1@gmail.com>
2022-05-07 11:45:52 -0400

    initial artist-signup layout with photo

--- client/src/pages/ArtistSignup.js
+++ client/src/pages/ArtistSignup.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./ArtistSignup.css";
+import Artist from "../assets/juanArtist.PNG";
 
 const ArtistSignup = () => {
   return (
     <section>
-      <p>Hellow World</p>
+      <div className="mainSection">
+        <div className="artistPhoto">
+          <img className="backgroundImage" src={Artist} alt="folk singer" />
+        </div>
+
+        <div className="coralBack"></div>
+        <div className="greyBack"></div>
+      </div>
     </section>
   );
 };
