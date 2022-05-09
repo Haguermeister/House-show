623a924b703e25eeeeb79e8de17fcdc04eff102d
Johnathan Reese <JohnathanReese8@gmail.com>
2022-05-09 18:41:58 -0400

    Fixed compiling issues for HostProfile & ArtistProfile

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import ArtistProfile from "../components/ArtistProfile";
-// import HostProfile from "../components/HostProfile";
+import ArtistProfile from "../components/ArtistProfile/ArtistProfile";
+// import HostProfile from "../components/HostProfile/HostProfile";
 
 const Account = () => {
 
@@ -8,8 +8,8 @@ const Account = () => {
 
   <div className="">
     We are going to crush this
-    {/* <ArtistProfile />
-    <HostProfile /> */}
+    <ArtistProfile />
+    {/* <HostProfile /> */}
   </div>
 
   );
